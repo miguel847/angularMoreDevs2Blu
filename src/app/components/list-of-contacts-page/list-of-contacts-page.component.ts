c39392e0d7c22a2588353e7cc3e7f39502eb3b4b
MIGUEL LONNI DO PRADO <miguel.prado@alunos.sc.senac.br>
2025-04-01 20:37:53 -0300

    Adress field and delete button

--- src/app/components/list-of-contacts-page/list-of-contacts-page.component.ts
+++ src/app/components/list-of-contacts-page/list-of-contacts-page.component.ts
@@ -10,16 +10,22 @@ import { ContactComponent } from "../contact/contact.component";
 })
 export class ListOfContactsPageComponent {
   addContact() : void {
-    let c1 = new contact("placeholder", "placeholder");
+    let c1 = new contact("placeholder", "placeholder", "placeholder");
     this.contacts.push(c1);
   }
-  contacts : contact[] = [new contact("Bill Gates", "criador do windows"), new contact("Julian Galop", "Criador do x-com"), new contact("Marcos", "O mais importante de todos")];
+  deleteContact(id : number) : void {
+    console.log(this.contacts.splice(id, 1));
+  }
+
+  contacts : contact[] = [new contact("Bill Gates", "criador do windows", "Desconhecido"), new contact("Julian Galop", "Criador do x-com", "EUA"), new contact("Marcos", "O mais importante de todos", "Reino das Bolinhas")];
 }
 class contact {
-  constructor(n : string, d : string){
+  constructor(n : string, d : string, a : string){
     this.name = n;
     this.description = d;
+    this.adress = a;
   }
   name : string = "Seu George";
   description : string = "Um cara legal";
+  adress! : string;
 }
